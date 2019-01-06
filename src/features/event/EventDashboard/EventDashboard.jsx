d71ac6d6cfe7b461dc453d11170eee1361f44138
aruyamam <aruyamam@gmail.com>
2019-01-06 11:29:04 +0900

    Adding the Event Form

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
 
 class EventDashboard extends Component {
-   render() {
-      return (
-         <Grid>
-            <Grid.Column width={10}>
-               <EventList />
-            </Grid.Column>
-            <Grid.Column width={6}>
-               <h2>Right Column</h2>
-            </Grid.Column>
-         </Grid>
-      );
-   }
+  render() {
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Button positive content="Create Event" />
+          <EventForm />
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default EventDashboard;
---
+++ src/features/event/EventForm/EventForm.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Segment, Form, Button } from 'semantic-ui-react';
+
+class EventForm extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Segment>
+        <Form>
+          <Form.Field>
+            <label htmlFor="firstname">
+              Event Title
+              <input type="text" id="firstname" placeholder="First Name" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="date">
+              Event Date
+              <input type="date" id="date" placeholder="Event Date" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="city">
+              City
+              <input type="text" id="city" placeholder="City event is taking place" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="venue">
+              Venue
+              <input type="text" id="venue" placeholder="Enter the Venue of the event" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="host">
+              Hosted By
+              <input type="text" id="host" placeholder="Enter the name of person hosting" />
+            </label>
+          </Form.Field>
+          <Button type="submit" positive>
+            Submit
+          </Button>
+          <Button type="button">Cancel</Button>
+        </Form>
+      </Segment>
+    );
+  }
+}
+
+export default EventForm;
