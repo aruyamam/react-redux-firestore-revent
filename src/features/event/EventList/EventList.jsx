121ddaceab3eb142736fb7abdf1d0a337d0ee682
aruyamam <aruyamam@gmail.com>
2019-01-07 11:06:32 +0900

    Passing down static props to components

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import EventListItem from './EventListItem';
 
 class EventList extends Component {
-   render() {
-      return (
-         <div>
-            <h1>Event List</h1>
-            <EventListItem />
-            <EventListItem />
-            <EventListItem />
-         </div>
-      );
-   }
+  render() {
+    const { events } = this.props;
+
+    return (
+      <div>
+        <h1>Event List</h1>
+        {events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default EventList;
