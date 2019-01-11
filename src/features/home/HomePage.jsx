56b782a68d1504587245504a2fe67ed78b4fc8c0
aruyamam <aruyamam@gmail.com>
2019-01-11 10:52:40 +0900

    Adding a home page

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 
-const HomePage = () => (
+const HomePage = ({ history }) => (
    <div>
-      <h1>Home Page</h1>
+      <div className="ui inverted vertical masthead center aligned segment">
+         <div className="ui text continer">
+            <h1 className="ui inverted stackable header">
+               <img src="/assets/logo.png" alt="logo" className="ui image massive" />
+               <div className="content">Re-vents</div>
+            </h1>
+            <h2>Do whatever you want to do</h2>
+            <button
+               onClick={() => history.push('/events')}
+               className="ui huge white inverted button"
+               type="button"
+            >
+               Get Started
+               <i className="right arrow icon" />
+            </button>
+         </div>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+         Icons made by
+         {' '}
+         <a href="http://www.freepik.com" title="Freepik">
+            Freepik
+         </a>
+         {' from '}
+         <a href="https://www.flticon.com/" title="Flaticon">
+            www.flaticon.com
+         </a>
+         {' is licensed by '}
+         <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">
+            CC 3.0 BY
+         </a>
+      </div>
    </div>
 );
 
