47c919a76e1cb2e6799d0ef813408e67975ab8fa
aruyamam <aruyamam@gmail.com>
2019-01-13 10:09:11 +0900

    Improving the dev experience

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,15 +1,27 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/rootReducer';
 
-export const configureStore = (preloadedState) => {
+const configureStore = (preloadedState) => {
    const middlewares = [];
    const middlewareEnhancer = applyMiddleware(...middlewares);
 
    const storeEnhancers = [middlewareEnhancer];
 
-   const composedEnhancer = compose(...storeEnhancers);
+   const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
    const store = createStore(rootReducer, preloadedState, composedEnhancer);
 
+   if (process.env.NODE_ENV !== 'production') {
+      if (module.hot) {
+         module.hot.accept('../reducers/rootReducer', () => {
+            const newRootReducer = require('../reducers/rootReducer').default;
+            store.replaceReducer(newRootReducer);
+         });
+      }
+   }
+
    return store;
 };
+
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
-import { configureStore } from './app/store/configureStore';
+import configureStore from './app/store/configureStore';
 
 const store = configureStore();
 
