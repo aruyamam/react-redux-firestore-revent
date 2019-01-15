577a2d258f18735bef4755f498ea6c05adb457f1
aruyamam <aruyamam@gmail.com>
2019-01-15 11:24:38 +0900

    Adding the Event detailed page info, sidebar and chat

--- src/features/events/EventDetailed/EventDetailedInfo.jsx
+++ src/features/events/EventDetailed/EventDetailedInfo.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import {
+   Button, Grid, Icon, Segment,
+} from 'semantic-ui-react';
 
 const EventDetailedInfo = () => (
-   <div>
-      <h1>Info</h1>
-   </div>
+   <Segment.Group>
+      <Segment>
+         <Grid>
+            <Grid.Column width={1}>
+               <Icon name="info" size="large" color="teal" />
+            </Grid.Column>
+            <Grid.Column width={15}>
+               <p>Description of Event</p>
+            </Grid.Column>
+         </Grid>
+      </Segment>
+      <Segment>
+         <Grid>
+            <Grid.Column width={1}>
+               <Icon name="calendar" size="large" color="teal" />
+            </Grid.Column>
+            <Grid.Column width={15}>
+               <span>Event Date</span>
+            </Grid.Column>
+         </Grid>
+      </Segment>
+      <Segment>
+         <Grid>
+            <Grid.Column width={1}>
+               <Icon name="marker" size="large" color="teal" />
+            </Grid.Column>
+            <Grid.Column width={11}>
+               <span>Event Venue</span>
+            </Grid.Column>
+            <Grid.Column width={4}>
+               <Button content="Show Map" size="tiny" color="teal" />
+            </Grid.Column>
+         </Grid>
+      </Segment>
+   </Segment.Group>
 );
 
 export default EventDetailedInfo;
--- src/features/events/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/events/EventDetailed/EventDetailedSidebar.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import {
+   Item, Label, List, Segment,
+} from 'semantic-ui-react';
 
 const EventDetailedSidebar = () => (
    <div>
-      <h1>Sidebar</h1>
+      <Segment
+         textAlign="center"
+         attached="top"
+         secondary
+         inverted
+         color="teal"
+         style={{ border: 'none' }}
+      >
+         2 People Going
+      </Segment>
+      <Segment attached>
+         <List relaxed divided>
+            <Item style={{ position: 'relative' }}>
+               <Label ribbon="right" color="orange" style={{ position: 'absolute' }}>
+                  Host
+               </Label>
+               <Item.Content verticalAlign="middle">
+                  <Item.Image src="/assets/user.png" size="tiny" />
+                  <Item.Header as="h3">
+                     <a>Attendee Name</a>
+                  </Item.Header>
+               </Item.Content>
+            </Item>
+         </List>
+      </Segment>
    </div>
 );
 
