755c5be76124b97e6a0185952eb4932afd66abfb
aruyamam <aruyamam@gmail.com>
2019-01-16 11:03:41 +0900

    Adding static data for the event detailed page

--- src/features/events/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/events/EventDetailed/EventDetailedSidebar.jsx
@@ -3,34 +3,56 @@ import {
    Item, Label, List, Segment,
 } from 'semantic-ui-react';
 
-const EventDetailedSidebar = () => (
-   <div>
-      <Segment
-         textAlign="center"
-         attached="top"
-         secondary
-         inverted
-         color="teal"
-         style={{ border: 'none' }}
-      >
-         2 People Going
-      </Segment>
-      <Segment attached>
-         <List relaxed divided>
-            <Item style={{ position: 'relative' }}>
-               <Label ribbon="right" color="orange" style={{ position: 'absolute' }}>
-                  Host
-               </Label>
-               <Item.Content verticalAlign="middle">
-                  <Item.Image src="/assets/user.png" size="tiny" />
-                  <Item.Header as="h3">
-                     <a>Attendee Name</a>
-                  </Item.Header>
-               </Item.Content>
-            </Item>
-         </List>
-      </Segment>
-   </div>
-);
+const itemContentStyle = {
+   display: 'flex',
+   alignItems: 'center',
+   flexDirection: 'row-reverse',
+   justifyContent: 'flex-end',
+};
+
+const EventDetailedSidebar = ({ attendees }) => {
+   const isHost = false;
+
+   return (
+      <div>
+         <Segment
+            textAlign="center"
+            attached="top"
+            secondary
+            inverted
+            color="teal"
+            style={{ border: 'none' }}
+         >
+            {attendees && attendees.length}
+            {' '}
+            {attendees && attendees.length === 1 ? 'Person' : 'People'}
+            {' '}
+Going
+         </Segment>
+         <Segment attached>
+            <List relaxed divided>
+               {attendees
+                  && attendees.map(attendee => (
+                     <Item style={{ position: 'relative' }}>
+                        <Label ribbon="right" color="orange" style={{ position: 'absolute' }}>
+                           Host
+                        </Label>
+                        <Item.Content verticalAlign="middle" style={itemContentStyle}>
+                           <Item.Header as="h3">
+                              <a>{attendee.name}</a>
+                           </Item.Header>
+                           <Item.Image
+                              src={attendee.photoURL}
+                              size="tiny"
+                              style={{ marginRight: '1rem' }}
+                           />
+                        </Item.Content>
+                     </Item>
+                  ))}
+            </List>
+         </Segment>
+      </div>
+   );
+};
 
 export default EventDetailedSidebar;
