75e95d45deceaf3bafed27a7ed379430fbc9fd00
aruyamam <aruyamam@gmail.com>
2019-01-18 11:15:42 +0900

    Creating a reusable Select input field

--- src/app/common/form/TextArea.jsx
+++ src/app/common/form/TextArea.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Form, Label } from 'semantic-ui-react';
 
 const TextArea = ({
-   input, rows, type, placeholder, meta: { touched, error },
+   input, rows, placeholder, meta: { touched, error },
 }) => (
    <Form.Field error={touched && !!error}>
       <textarea {...input} placeholder={placeholder} rows={rows} />
