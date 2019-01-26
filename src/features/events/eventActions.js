4c272acf3ea5844a7a99834cbf56fcacd6770e0c
aruyamam <aruyamam@gmail.com>
2019-01-26 11:44:14 +0900

    Fetching events from the mock API

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -1,4 +1,13 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from './eventConstants';
+import {
+   CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT, FETCH_EVENTS,
+} from './eventConstants';
+import { asyncActionStart, asyncActionFinish, asyncActionError } from '../async/asyncActions';
+import fetchSampleData from '../../app/data/mockApi';
+
+export const fetchEvents = events => ({
+   type: FETCH_EVENTS,
+   payload: events,
+});
 
 export const createEvent = event => ({
    type: CREATE_EVENT,
@@ -20,3 +29,16 @@ export const deleteEvent = eventId => ({
       eventId,
    },
 });
+
+export const loadEvent = () => async (dispatch) => {
+   try {
+      dispatch(asyncActionStart());
+      const events = await fetchSampleData();
+      dispatch(fetchEvents(events));
+      dispatch(asyncActionFinish());
+   }
+   catch (error) {
+      console.log(error);
+      dispatch(asyncActionError());
+   }
+};
