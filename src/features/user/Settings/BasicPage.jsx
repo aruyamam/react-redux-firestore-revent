b945aadfd8a1d65fa4af60a1d10a1045387bd343
aruyamam <aruyamam@gmail.com>
2019-02-03 11:34:06 +0900

    Setting up the basic user page form

--- src/features/user/Settings/BasicPage.jsx
+++ src/features/user/Settings/BasicPage.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+   Button, Divider, Form, Header, Segment,
+} from 'semantic-ui-react';
+import { Field, reduxForm } from 'redux-form';
+import DateInput from '../../../app/common/form/DateInput';
+import PlaceInput from '../../../app/common/form/PlaceInput';
+import TextInput from '../../../app/common/form/TextInput';
 
-const BasicPage = () => (
-   <div>
-      <h1>Basic Page</h1>
-   </div>
-);
+class BasicPage extends Component {
+   state = {};
 
-export default BasicPage;
+   render() {
+      const { pristine, submitting } = this.props;
+
+      return (
+         <Segment>
+            <Header content="Basics" dividing size="large" />
+            <Form>
+               <Field
+                  component={TextInput}
+                  name="displayName"
+                  type="text"
+                  placeholder="Known As"
+                  width={8}
+               />
+               <Field
+                  component={DateInput}
+                  name="dateOfBirth"
+                  placeholder="Date of the Birth"
+                  width={8}
+               />
+               <Field
+                  component={PlaceInput}
+                  label="Female"
+                  name="city"
+                  options={{ types: ['(cities)'] }}
+                  placeholder="Home Town"
+                  width={8}
+               />
+               <Divider />
+               <Button
+                  content="Update Profile"
+                  disabled={pristine || submitting}
+                  positive
+                  size="large"
+               />
+            </Form>
+         </Segment>
+      );
+   }
+}
+
+export default reduxForm({ form: 'userProfile', enableReinitialize: true })(BasicPage);
