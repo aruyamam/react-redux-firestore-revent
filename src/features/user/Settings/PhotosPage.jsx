d45a3e1deb0ef5cde6558d753fae947fa44bde68
aruyamam <aruyamam@gmail.com>
2019-02-06 12:07:56 +0900

    React Dropzone

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import {
-   Button, Card, Divider, Grid, Header, Image, Segment,
+   Button, Card, Divider, Grid, Header, Icon, Image, Segment,
 } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
+
+const dropZoneStyle = {
+   border: '1px dashed black',
+   borderRadius: '5px',
+   padding: '10px 0',
+   textAlign: 'center',
+};
+
+const dropZoneStyleActive = {
+   opacity: '0.5',
+};
+
+const ImagePreviewStyle = {
+   minHeight: '200px',
+   minWidth: '200px',
+};
 
 class PhotosPage extends Component {
-   state = {};
+   state = {
+      files: [],
+      fileName: '',
+   };
+
+   onDrop = (files) => {
+      this.setState({
+         files: files.map(file => Object.assign(file, { preview: URL.createObjectURL(file) })),
+         fileName: files[0].name,
+      });
+   };
 
    render() {
+      const { files } = this.state;
+
       return (
          <Segment>
             <Header content="Your Photos" dividing size="large" />
@@ -14,6 +43,20 @@ class PhotosPage extends Component {
                <Grid.Row />
                <Grid.Column width={4}>
                   <Header color="teal" content="Step 1 - Add Photo" />
+                  <Dropzone onDrop={this.onDrop} multiple={false}>
+                     {({ getRootProps, isDragActive }) => {
+                        const style = isDragActive
+                           ? { ...dropZoneStyle, ...dropZoneStyleActive }
+                           : dropZoneStyle;
+
+                        return (
+                           <div {...getRootProps()} style={style}>
+                              <Icon name="upload" size="huge" />
+                              <Header content="Drop image here or click to add" />
+                           </div>
+                        );
+                     }}
+                  </Dropzone>
                </Grid.Column>
                <Grid.Column width={1} />
                <Grid.Column width={4}>
@@ -22,6 +65,7 @@ class PhotosPage extends Component {
                <Grid.Column width={1} />
                <Grid.Column width={4}>
                   <Header color="teal" content="Step 3 - Preview and Upload" />
+                  <Image style={ImagePreviewStyle} src={files[0] && files[0].preview} />
                </Grid.Column>
             </Grid>
             <Divider />
