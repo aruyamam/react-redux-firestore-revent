000a73dbeaf06a1faa305b5b7b247f0788245f00
aruyamam <aruyamam@gmail.com>
2019-02-11 11:30:21 +0900

    UserDetailedPage Component

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Menu, Dropdown, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-const SignedInMenu = ({ signOut, profile }) => (
+const SignedInMenu = ({ signOut, profile, auth }) => (
    <Menu.Item position="right">
       <Image avatar spaced="right" src={profile.photoURL || '/assets/user.png'} />
       <Dropdown pointing="top left" text={profile.displayName}>
@@ -11,7 +11,7 @@ const SignedInMenu = ({ signOut, profile }) => (
             <Dropdown.Item text="Create Event" icon="plus" />
             <Dropdown.Item text="My Events" icon="calendar" />
             <Dropdown.Item text="My Network" icon="users" />
-            <Dropdown.Item text="My Profile" icon="user" />
+            <Dropdown.Item as={Link} to={`/profile/${auth.uid}`} text="My Profile" icon="user" />
             <Dropdown.Item as={Link} to="/settings" text="Settings" icon="settings" />
             <Dropdown.Item onClick={signOut} text="Sing Out" icon="power" />
          </Dropdown.Menu>
