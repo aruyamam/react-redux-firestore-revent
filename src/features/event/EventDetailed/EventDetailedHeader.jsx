08a6724a73253ba16de5df390ee7da5fe8faf9df
aruyamam <aruyamam@gmail.com>
2019-02-15 11:28:04 +0900

    Fixed date problem & adding prop validation

--- src/features/event/EventDetailed/EventDetailedHeader.jsx
+++ src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import format from 'date-fns/format';
 import {
@@ -18,40 +19,57 @@ const eventImageTextStyle = {
    color: 'white',
 };
 
-const EventDetailedHeader = ({ event }) => (
-   <Segment.Group>
-      <Segment basic attached="top" style={{ padding: '0' }}>
-         <Image
-            src={`/assets/categoryImages/${event.category}.jpg`}
-            fluid
-            style={eventImageStyle}
-         />
-         <Segment basic style={eventImageTextStyle}>
-            <Item.Group>
-               <Item>
-                  <Item.Content>
-                     <Header size="huge" content={event.title} style={{ color: 'white' }} />
-                     <p>{format(event.date, 'dddd Do MMMM')}</p>
-                     <p>
-                        Hosted by
-                        {' '}
-                        <strong>{event.hostedBy}</strong>
-                     </p>
-                  </Item.Content>
-               </Item>
-            </Item.Group>
+const EventDetailedHeader = ({ event }) => {
+   let eventDate;
+   if (event.date) {
+      eventDate = event.date.toDate();
+   }
+
+   return (
+      <Segment.Group>
+         <Segment basic attached="top" style={{ padding: '0' }}>
+            <Image
+               src={`/assets/categoryImages/${event.category}.jpg`}
+               fluid
+               style={eventImageStyle}
+            />
+            <Segment basic style={eventImageTextStyle}>
+               <Item.Group>
+                  <Item>
+                     <Item.Content>
+                        <Header size="huge" content={event.title} style={{ color: 'white' }} />
+                        <p>{format(eventDate, 'dddd Do MMMM')}</p>
+                        <p>
+                           Hosted by
+                           {' '}
+                           <strong>{event.hostedBy}</strong>
+                        </p>
+                     </Item.Content>
+                  </Item>
+               </Item.Group>
+            </Segment>
          </Segment>
-      </Segment>
 
-      <Segment attached="bottom">
-         <Button>Cancel My Place</Button>
-         <Button color="teal">JOIN THIS EVENT</Button>
+         <Segment attached="bottom">
+            <Button>Cancel My Place</Button>
+            <Button color="teal">JOIN THIS EVENT</Button>
 
-         <Button as={Link} to={`/manage/${event.id}`} color="orange" floated="right">
-            Manage Event
-         </Button>
-      </Segment>
-   </Segment.Group>
-);
+            <Button as={Link} to={`/manage/${event.id}`} color="orange" floated="right">
+               Manage Event
+            </Button>
+         </Segment>
+      </Segment.Group>
+   );
+};
+
+EventDetailedHeader.propTypes = {
+   event: PropTypes.shape({
+      category: PropTypes.string.isRequired,
+      date: PropTypes.object.isRequired,
+      hostedBy: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+   }).isRequired,
+};
 
 export default EventDetailedHeader;
