dbf8d5eeb95c06bbf41f130e08fa546406a12903
aruyamam <aruyamam@gmail.com>
2019-02-28 11:16:13 +0900

    Adding the React lazy load

--- src/features/user/UserDetailed/UserDetailedPhotos.jsx
+++ src/features/user/UserDetailed/UserDetailedPhotos.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
    Grid, Header, Image, Segment,
 } from 'semantic-ui-react';
+import Lazyload from 'react-lazyload';
 
 const UserDetailedPhotos = ({ photos }) => (
    <Grid.Column width={12}>
@@ -9,11 +11,22 @@ const UserDetailedPhotos = ({ photos }) => (
          <Header content="Photos" icon="image outline" />
          <Image.Group size="small">
             {photos.map(photo => (
-               <Image key={photo.id} src={photo.url} />
+               <Lazyload key={photo.id} height={150} placeholder={<Image src="/assets/user.png" />}>
+                  <Image src={photo.url} />
+               </Lazyload>
             ))}
          </Image.Group>
       </Segment>
    </Grid.Column>
 );
+
+UserDetailedPhotos.propTypes = {
+   photos: PropTypes.arrayOf(
+      PropTypes.shape({
+         id: PropTypes.string,
+         url: PropTypes.string,
+      }),
+   ).isRequired,
+};
 
 export default UserDetailedPhotos;
